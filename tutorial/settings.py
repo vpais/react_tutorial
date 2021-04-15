156972ee49e043ae0a5b7adf80b5fe13e90b4958
Vasco Pais <v.pais@runtime-revolution.com>
2021-04-15 10:16:36 +0100

    Add relationships and hyperlinked APIs

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'snippets.apps.SnippetsConfig',
+    'django_extensions',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
